48d787318a6fef95bf01c756bd15b42f5864d59e
linh2005-S <huynhnguyennhutlinh7311@gmail.com>
2024-08-25 11:49:18 +0700

    Add files via upload

--- tachđhcach2.py
+++ tachđhcach2.py
@@ -5,7 +5,7 @@ Created on Sun Aug 25 10:14:53 2024
 @author: HUYNHNGUYENHUTLINH
 """
 chuoi="Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
-mot= chuoi.split(" , ")
-hai= chuoi.replace("P. ","").replace("Q. ","").split(" , ")
-print(mot)
-print(hai)
+chuoi=chuoi.replace("P. ","").replace("Q. ","").replace("Tp. ","")
+sub_string=chuoi.split(", ")
+for sub in sub_string:
+    print(sub)
